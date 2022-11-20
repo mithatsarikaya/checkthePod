0fe7eb1357d8921c7bc7ac9004647180e4ccf8af
mithatsarikaya <mithatsarikaya58@gmail.com>
2022-11-20 22:11:15 +0300

    decimal sayıların görüntüsü düzeltildi

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const {requireAuth} = require('../middlewares/authMiddleware')
 
 router.get('/', userController.loginIndex)
 router.get('/add',requireAuth, userController.userAddGet)
-router.post('/add',requireAuth, userController.userAddPost)
+router.post('/add',requireAuth ,userController.userAddPost)
 router.post('/post', userController.loginPost)
 router.get('/logout', userController.logoutGet)
 
